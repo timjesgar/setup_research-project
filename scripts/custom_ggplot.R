09a052a972a27af2aa7a576a2585d07dc82de832
timjesgar <tim.jesgarzewsky@live.de>
2025-11-04 17:06:41 +0100

    add folder and file setup with examples

---
+++ scripts/custom_ggplot.R
@@ -0,0 +1,20 @@
+# load libs
+library(ggplot2)
+
+# set new defaults
+
+## theme
+theme_set(theme_classic())
+
+## colors
+options(
+  ggplot2.discrete.colour = c(
+    "#E69F00",
+    "#56B4E9",
+    "#009E73",
+    "#F0E442",
+    "#0072B2",
+    "#D55E00",
+    "#CC79A7"
+  )
+)
